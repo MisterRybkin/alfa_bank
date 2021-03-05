b21e51e8fad1aa60f0cfe83a3ffc436bd60a8237
MisterRybkin <dyxa145@gmail.com>
2021-03-05 16:34:41 +0700

    update and create html

--- src/main/java/ru/rybkin/alfa_bank/controllers/RatesController.java
+++ src/main/java/ru/rybkin/alfa_bank/controllers/RatesController.java
@@ -2,13 +2,11 @@ package ru.rybkin.alfa_bank.controllers;
 
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.rybkin.alfa_bank.services.RatesService;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping(value = "rates")
 public class RatesController {
 
